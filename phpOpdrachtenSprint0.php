7cd392ce387aeabb7128e70d5811d84afea0b545
Christian <118530836+faze-chris@users.noreply.github.com>
2023-09-06 09:06:24 +0200

    update

--- phpOpdrachtenSprint0.php
+++ phpOpdrachtenSprint0.php
@@ -1,49 +1,43 @@
 <?php
-  /* ----------- Database Delete ---------- */
-
-/* 
-  Delete a row in the database.
-*/
-
-
 date_default_timezone_set("europe/amsterdam");
 $today = date("j F Y");
+$time = date("z");
+$dayOfTheWeek = date("l");
+$numberOfTheWeek = date("N");
+$time = date("H:I");
+$month = date("F");
+$daysInMonth = date("t");
+$year = date(" Y");
+//$week = date("w");
 
 echo "Het is vandaag : $today";
 
 echo "<br>";
 
-$time = date("z");
-
 echo "vandaag is het de $time dag van het jaar.";
 
 echo "<br>";
-$dayOfTheWeek = date("l");
-$numberOfTheWeek = date("N");
 
 echo "$dayOfTheWeek is het de $numberOfTheWeek dag van de week.";
 
 echo "<br>";
 
-$time = date("H:I");
-
 echo "het is nu: $time uur";
 
 echo "<br>";
 
-$month = date("F");
-
-$daysInMonth = date("t");
 echo "de maand $month heeft in totaal $daysInMonth dagen";
 
 echo "<br>";
 
-//$week = date("w");
-//
 //echo "deze week is het week: $week <br>";
 
-$year = date(" Y");
+//echo "het jaar $year is geen schrikkeljaar";
 
-echo "het jaar $year is geen schrikkeljaar";
+if (date ("L") == 1){
+    echo "het jaar " . date("Y") . " is een schrikkeljaar.";
+}else{
+    echo "het jaar " . date("Y") . " is geen schrikkeljaar.";
+}
 
 ?>
